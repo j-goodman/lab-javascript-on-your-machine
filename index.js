a9289c35fa4d34225ff451a1fad5a770bb884779
j-goodman <johngoodman@pursuit.org>
2023-01-17 16:34:54 -0500

    Wrote calculator function.

--- index.js
+++ index.js
@@ -4,7 +4,27 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let operation = process.argv[2]
+    let result = parseInt(process.argv[3])
+    if (!process.argv[2]) {
+        return "No operation provided..."
+    }
+    if (!process.argv[3]) {
+        return "No numbers provided..."
+    }
+    for (let i = 4; i < process.argv.length; i++) {
+        let number = parseInt(process.argv[i])
+        if (operation === "plus") {
+            result += number;
+        } else if (operation === "minus") {
+            result -= number;
+        } else {
+            result = `Invalid operation: ${operation}`
+        }
+    }
+    return result
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
